1f0a54a35b918dac0f78ed0b5e0f56ca50e1ec2f
TEY WEI YEH 2201711E <2201711E@student.tp.edu.sg>
2024-01-03 17:32:01 +0800

    Revert "Revert "Added migration and some seeding""
    This reverts commit bba26bd130e57cae1fe619b892ad0b116da2bb15.

---
+++ ProductReview/Shared/Domain/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReview.Shared.Domain
+{
+    public class Comment : BaseDomainModel
+    {
+        public string? Description { get; set; } = String.Empty;
+        public int ReviewId { get; set; }
+        public virtual Review? Review { get; set; }
+
+    }
+}
