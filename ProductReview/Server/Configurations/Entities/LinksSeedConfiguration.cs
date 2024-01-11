0ffd9a470756e876610c75443b9e71ca0af679a6
TEY WEI YEH 2201711E <2201711E@student.tp.edu.sg>
2024-01-11 11:47:06 +0800

    Added Links and Products seed

---
+++ ProductReview/Server/Configurations/Entities/LinksSeedConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductReview.Shared.Domain;
+
+namespace ProductReview.Server.Configurations.Entities
+{
+    public class LinksSeedConfiguration : IEntityTypeConfiguration<Link>
+    {
+        public void Configure(EntityTypeBuilder<Link> builder)
+        {
+            builder.HasData(
+                new Link
+                {
+                    Id = 1,
+                    PlatformName = "Amazon",
+                    LinkUrl = "https://www.amazon.com/New-Simple-Mobile-Prepaid-Product/dp/B0875RK26R/ref=sr_1_1?keywords=iPhone+SE&qid=1704944545&sr=8-1",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Link
+                {
+                    Id = 2,
+                    PlatformName = "Apple",
+                    LinkUrl = "https://www.apple.com/iphone-se/",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                }
+                );
+        }
+    }
+}
---
+++ ProductReview/Server/Configurations/Entities/ProductsSeedConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductReview.Shared.Domain;
+
+namespace ProductReview.Server.Configurations.Entities
+{
+    public class ProductsSeedConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "Iphone SE",
+                    Description = "Iphone SE...",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Iphone 13 ",
+                    Description = "Iphone 13",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                }
+            );
+        }
+
+
+    }
+}
