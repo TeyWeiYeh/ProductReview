4996b8f0b42f768e693d3f04f8b3dd35069ed9c2
TEY WEI YEH 2201711E <2201711E@student.tp.edu.sg>
2024-02-03 22:54:39 +0800

    Added function to refresh page after action

--- ProductReview/Client/wwwroot/js/reloadPage.js
+++ ProductReview/Client/wwwroot/js/reloadPage.js
@@ -1,10 +1,3 @@
-﻿function reloadPageAndNavigate(url) {
+﻿function reloadPage() {
     location.reload();
-    setTimeout(function () {
-        window.location.href = url;
-    }, 2000); //set delay of 100ms
-}
-
-function reloadApp() {
-    window.location.reload(true);
 }
